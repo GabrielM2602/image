39a46599be3f51bbfd1411665275903c8695380e
GabrielM <gabriemdo26@gmail.com>
2026-04-23 10:54:15 -0400

    2304

--- image/demo/src/main/java/io/spring/image/demo/ImageApplication.java
+++ image/demo/src/main/java/io/spring/image/demo/ImageApplication.java
@@ -4,7 +4,6 @@ import io.spring.image.demo.domain.entity.Image;
 import io.spring.image.demo.domain.enums.ImageExtension;
 import io.spring.image.demo.infra.repository.ImageRepository;
 import org.aspectj.apache.bcel.Repository;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,24 +15,23 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class ImageApplication {
 
-	@Bean
-	public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
-		return args -> {
-			Image image = Image
-					.builder()
-					.extension(ImageExtension.JPEG)
-					.name("myimage")
-					.tags("teste")
-					.size(1000L)
-					.build();
-			repository.save(image);
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
+//		return args -> {
+//			Image image = Image
+//					.builder()
+//					.extension(ImageExtension.JPEG)
+//					.name("myimage")
+//					.tags("teste")
+//					.size(1000L)
+//					.build();
+//			repository.save(image);
+//
+//		};
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImageApplication.class, args);
 	}
 
 }
-
